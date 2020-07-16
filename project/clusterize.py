cc3e6cd4d1cf2eb5d1c474182f19968ba437ae75
Eric Imai <ericimai@hotmail.com>
2020-07-16 14:06:21 -0300

    clusterize_share update

--- project/clusterize.py
+++ project/clusterize.py
@@ -17,60 +17,51 @@ def elbow_method(X):
 	visualizer.fit(X)
 	return visualizer.elbow_value_
 
-def clusterize_structure(X):
+def clusterize(X):
 	df_x = pd.DataFrame(X)
 	optimal_k = elbow_method(X)
 	kmeans = KMeans(n_clusters=optimal_k)
+	print('Parameters: \n')
+	return kmeans, optimal_k
+
+def clusterize_structure(X):
+	df_x = pd.DataFrame(X)
+	kmeans, optimal = clusterize(X)
 	df_x["clusters"] = kmeans.fit_predict(df_x.values)
 	df_x.groupby("clusters").aggregate("mean").plot.bar()
 	plt.show()
 
 def clusterize_share(X):
+	cluster_pool = []
 	df_x = pd.DataFrame(X)
-	optimal_k = elbow_method(X)
-	kmeans = KMeans(n_clusters=optimal_k)
-	df_x["clusters"] = kmeans.fit_predict(df_x.values)
-	sample_0 = 0
-	sample_1 = 0
-	sample_2 = 0
-	sample_3 = 0
-	sample_4 = 0
-	for sample in kmeans.predict(X):
-		if sample == 0:
-			sample_0 = sample_0 + 1
-		elif sample == 1:
-			sample_1 = sample_1 + 1
-		elif sample == 2:
-			sample_2 = sample_2 + 1		
-		elif sample == 3:
-			sample_3 = sample_3 + 1
-		elif sample == 4:
-			sample_4 = sample_4 + 1
-	print('\n')
-	print('Total:',len(kmeans.predict(X)),'\n')
-	print('Share:\n')
-	c0_porc = 100*sample_0/len(kmeans.predict(X))
-	print(c0_porc ,'%')
-	c1_porc = 100*sample_1/len(kmeans.predict(X))
-	print(c1_porc ,'%')
-	c2_porc = 100*sample_2/len(kmeans.predict(X))
-	print(c2_porc ,'%')
-	c3_porc = 100*sample_3/len(kmeans.predict(X))
-	print(c3_porc ,'%')
-	c4_porc = 100*sample_4/len(kmeans.predict(X))
-	print(c4_porc ,'%\n')
-
-	share = [c0_porc,c1_porc,c2_porc,c3_porc,c4_porc]
-	x = np.arange(optimal_k)
-	width = 0.35
-	fig, ax, = plt.subplots()
-	ax.bar(x, share, width, label = "cluster")
-	ax.set_xlabel('porcent')
-	ax.set_xlabel('cluster')
-	ax.set_title('Share')
-	ax.legend()
-	plt.show()
+	kmeans, optimal_k = clusterize(X)
+	predict = kmeans.fit_predict(df_x.values)
+	df_x["clusters"] = predict
+	for cluster_sample in range(optimal_k):
+		cluster_pool.append(0)
+	for cluster in range(len(cluster_pool)):
+		for sample in predict:
+			if sample == cluster:
+				cluster_pool[cluster] = cluster_pool[cluster] + 1
+	print('\nTotal Sample: ', len(predict))
+	print('Cluster_pool',cluster_pool)
+	
+	for id in range(len(cluster_pool)):
+		cluster_pool[id] = cluster_pool[id]/len(predict)
+		print('Cluster',id,':', '%.2f' % cluster_pool[id], '%')
+	# share = [c0_porc,c1_porc,c2_porc,c3_porc,c4_porc]
+	# x = np.arange(optimal_k)
+	# width = 0.35
+	# fig, ax, = plt.subplots()
+	# ax.bar(x, share, width, label = "cluster")
+	# ax.set_xlabel('porcent')
+	# ax.set_xlabel('cluster')
+	# ax.set_title('Share')
+	# ax.legend()
+	# plt.show()
 
 	return(kmeans.predict(X))
 
+# clusterize(word_ebbending.get_comment_vector(import_data.import_data()))
+# clusterize_structure(word_ebbending.get_comment_vector(import_data.import_data()))
 clusterize_share(word_ebbending.get_comment_vector(import_data.import_data()))
